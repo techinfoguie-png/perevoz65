8e1aad684a09e6a7ba562b0eb49e31e5f423d6a3
techinfoguie-png <techinfoguie@gmail.com>
2026-05-19 22:36:57 +1100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -378,7 +378,7 @@ function initMap() {
         ymaps.ready(function() {
             const coords = cmsData.map_coords ? 
                 cmsData.map_coords.value.split(',').map(Number) : 
-                [142.740752, 46.955526];
+                [142.7407, 46.9555];
 
             const map = new ymaps.Map('yandex-map', {
                 center: coords,
